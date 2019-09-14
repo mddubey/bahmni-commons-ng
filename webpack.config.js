7772b2bbddbedf70d8764e6e8fa9b8ec3465002f
MD Dubey <mddubey409@gmail.com>
2019-09-14 19:14:21 +0530

    Dubey | Extract Bahmni Util Commons; Independent module

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+
+module.exports = {
+    entry: {
+        "bahmni-util-commons": path.join(__dirname, 'src', 'bahmni-util-commons/init.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                // https://github.com/webpack/webpack/issues/512#issuecomment-288143187
+                // test: /[\\\/]lib[\\\/]modernizr*\.js$/,
+                test: /modernizr.custom/,
+                use: [
+                    { loader: 'imports-loader?this=>window'}
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: [
+                    { loader: 'babel-loader' }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i, 
+                loader: "url-loader",
+                options: {
+                    limit: Infinity // everything
+                  }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            }
+        ]
+    }
+};
